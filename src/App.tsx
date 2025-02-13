1a354bebcd8bde42b39a3361322a06db0ae7eb52
Tony <hoanganhtran1998@gmail.com>
2025-02-13 11:29:59 -0500

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import './App.css'
-import Home from './routes/Home'
 import TranscriptionPage from './routes/TranscriptionPage'
 import { ImageProvider } from './context/ImageContext'; // Import your context
 
@@ -7,7 +6,6 @@ function App() {
   return (
     <>
       <ImageProvider>
-        {/* <Home/> */}
         <TranscriptionPage />
       </ImageProvider>
     </>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css'
 import App from './App.tsx'
+import Home from './routes/Home'
+
+
+const Main = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/transcriber" element={<App/>} />
+      </Routes>
+    </Router>
+  );
+};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Main />
   </StrictMode>,
-)
+);
