fcf59bdc07404efec30badfcdced3381dce0438d
Tony <hoanganhtran1998@gmail.com>
2025-02-14 11:01:56 -0500

    responsive render home page

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -4,18 +4,21 @@ import Introduction from "../components/Introduction";
 import PhotoUploadMobile from "../components/PhotoUploadMobile";
 import TranscriptionTips from "../components/TranscriptionTips";
 import UploadImages from "../components/UploadImages";
+import { useMediaQuery } from "react-responsive";
 
 function Home() {
-    return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '40px', height: '100vh' }}> {/* Added height for vertical centering */}
-            {/* <div style={{ width: '948px', height: '835px', padding: '40px', background: 'white', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '40px' }}>
-                <Introduction />
-                <UploadImages />
-            </div>
-            <TranscriptionTips /> */}
-            <PhotoUploadMobile/>
-        </div>
-    );
+    const isMobile = useMediaQuery({ maxWidth: 768 });
+
+    return isMobile ? <PhotoUploadMobile /> : <PhotoUploadDesktop />;
 }
+
+const PhotoUploadDesktop = () =>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '40px', height: '100vh' }}> {/* Added height for vertical centering */}
+        <div style={{ width: '948px', height: '835px', padding: '40px', background: 'white', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '40px' }}>
+            <Introduction />
+            <UploadImages />
+        </div>
+        <TranscriptionTips />
+    </div>
 
 export default Home;
