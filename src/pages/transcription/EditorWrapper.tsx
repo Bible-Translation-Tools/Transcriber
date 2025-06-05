2fd9465dd6c71e63b8b751c7e349957bb83dfd71
Joe <jsarabia1247@gmail.com>
2025-06-04 20:18:49 -0400

    Extract out TranscriptionWorkspace to Component

--- src/pages/transcription/EditorWrapper.tsx
+++ src/pages/transcription/EditorWrapper.tsx
@@ -1,12 +1,7 @@
-import RangeInput from "@components/forms/RangeInput.tsx";
-import TextEditor from "@components/forms/TextEditor.tsx";
 import Pagination from "@components/image/Pagination.tsx";
-import { ShowWhen } from "@components/utils/ShowWhen.tsx";
-import type { TranscribableDocument } from "@src/data/TranscribableDocument.ts";
-import { TranscriptionStatus } from "@src/data/TranscriptionStatus.ts";
+import type {TranscribableDocument} from "@src/data/TranscribableDocument.ts";
 import EmptyProject from "@src/pages/transcription/EmptyProject.tsx";
-import TranscriptionStatusOverlay from "@src/pages/transcription/TranscriptionStatusOverlay.tsx";
-import { useTranslation } from "react-i18next";
+import {TranscriptionWorkspace} from "@src/pages/transcription/TranscriptionWorkspace.tsx";
 
 type EditorWrapperProps = {
 	images: TranscribableDocument[] | undefined;
@@ -23,8 +18,6 @@ export default function EditorWrapper({
 	handleTextChange,
 	handleVerseRangeChange,
 }: EditorWrapperProps) {
-	const { t } = useTranslation();
-
 	if (!images || images.length === 0) {
 		return <EmptyProject />;
 	}
@@ -35,55 +28,17 @@ export default function EditorWrapper({
 				image={selectedImage}
 				onRetryTranscription={handleResubmitImage}
 			/>
-			<div className="flex-2 p-4">
-				<div className="h-full flex flex-col">
-					<ShowWhen when={!!selectedImage}>
-						<RangeInput
-							key={selectedImage?.id}
-							selectedImage={selectedImage}
-							onRangeChange={handleVerseRangeChange}
-						/>
-					</ShowWhen>
-					<div className="h-full flex-1 flex-col">
-						<ShowWhen
-							when={
-								selectedImage?.status ===
-								TranscriptionStatus.IN_PROGRESS
-							}
-						>
-							<TranscriptionStatusOverlay
-								mainMessage={t(
-									"This Image is Being Processed.",
-								)}
-								subMessage={t("Please Be Patient message")}
-							/>
-						</ShowWhen>
-						<ShowWhen
-							when={
-								selectedImage?.status ===
-								TranscriptionStatus.TRANSCRIPTION_ERROR
-							}
-						>
-							<TranscriptionStatusOverlay
-								mainMessage={t("Transcription Errored")}
-								subMessage={t("Please Retry Message")}
-							/>
-						</ShowWhen>
-						<ShowWhen
-							when={
-								selectedImage?.status ===
-								TranscriptionStatus.COMPLETED
-							}
-						>
-							<TextEditor
-								text={selectedImage?.transcription ?? ""}
-								onChange={(text) => {
-									handleTextChange(text);
-								}}
-							/>
-						</ShowWhen>
-					</div>
-				</div>
+			<div className="flex-2">
+				<TranscriptionWorkspace
+					key={selectedImage?.id}
+					selectedImage={selectedImage}
+					onRangeChange={handleVerseRangeChange}
+					status={selectedImage?.status}
+					transcription={selectedImage?.transcription}
+					onChange={(text) => {
+						handleTextChange(text);
+					}}
+				/>
 			</div>
 		</div>
 	);
---
+++ src/pages/transcription/TranscriptionWorkspace.tsx
@@ -0,0 +1,54 @@
+import RangeInput from "@components/forms/RangeInput.tsx";
+import TextEditor from "@components/forms/TextEditor.tsx";
+import {ShowWhen} from "@components/utils/ShowWhen.tsx";
+import type {TranscribableDocument} from "@src/data/TranscribableDocument.ts";
+import {TranscriptionStatus} from "@src/data/TranscriptionStatus.ts";
+import TranscriptionStatusOverlay from "@src/pages/transcription/TranscriptionStatusOverlay.tsx";
+import {useTranslation} from "react-i18next";
+
+export function TranscriptionWorkspace(props: {
+	selectedImage: TranscribableDocument | null;
+	onRangeChange: (start: number, end: number) => void;
+	status: TranscriptionStatus | undefined;
+	transcription: string | undefined | null;
+	onChange: (text: string) => void;
+}) {
+	const { t } = useTranslation();
+
+	return (
+		<div className="h-full flex flex-col p-4">
+			<ShowWhen when={!!props.selectedImage}>
+				<RangeInput
+					selectedImage={props.selectedImage}
+					onRangeChange={props.onRangeChange}
+				/>
+			</ShowWhen>
+			<div className="h-full flex-1 flex-col">
+				<ShowWhen
+					when={props.status === TranscriptionStatus.IN_PROGRESS}
+				>
+					<TranscriptionStatusOverlay
+						mainMessage={t("This Image is Being Processed.")}
+						subMessage={t("Please Be Patient message")}
+					/>
+				</ShowWhen>
+				<ShowWhen
+					when={
+						props.status === TranscriptionStatus.TRANSCRIPTION_ERROR
+					}
+				>
+					<TranscriptionStatusOverlay
+						mainMessage={t("Transcription Errored")}
+						subMessage={t("Please Retry Message")}
+					/>
+				</ShowWhen>
+				<ShowWhen when={props.status === TranscriptionStatus.COMPLETED}>
+					<TextEditor
+						text={props.transcription ?? ""}
+						onChange={props.onChange}
+					/>
+				</ShowWhen>
+			</div>
+		</div>
+	);
+}
